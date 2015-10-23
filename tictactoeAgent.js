7941529ecd15bce74f0c9eecc32f2cdb5f4e71c5
Jeffrey LeCompte <jeffreylecompte@protonmail.com>
2015-10-22 19:27:12 -0700

    playerOne logic is perfect. Will win or tie every game.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -8,7 +8,8 @@ var Agent = function () {
 
 Agent.prototype.selectMove = function(board) {
     var freeCells = [];
-    var k, l, f, i, j;
+    var evenNums = [2, 4, 6 ,8];
+    var f, i, j, k, l;
     for (i = 1; i < 10; i++) {
         if (board.cellFree(i)) freeCells.push(i);
     }
@@ -29,76 +30,71 @@ Agent.prototype.selectMove = function(board) {
             }
         }
 
-        if (board.O.length >= 2) {
-            // O:[1,5,7] free:[3,9,2,4]
-            for (k = 0; k < board.O.length - 1; k++) {
-                for (l = k + 1; l < board.O.length; l++) {
-                    for (f = 0; f < freeCells.length; f++) {
-                        magicSum = board.O[k] + board.O[l] + freeCells[f];
-                        if (magicSum === 15) {
-                            alert("Blocked!");
-                            return freeCells[f];
+        // test to see if we can block
+        for (k = 0; k < board.O.length - 1; k++) {
+            for (l = k + 1; l < board.O.length; l++) {
+                for (f = 0; f < freeCells.length; f++) {
+                    magicSum = board.O[k] + board.O[l] + freeCells[f];
+                    if (magicSum === 15) {
+                        alert("Blocked!");
+                        return freeCells[f];
+                    }
+                }
+            }
+        }
+
+        // nothing on the board, x makes random move in corner
+        if (freeCells.length === 9) {
+            return evenNums[Math.floor(Math.random() * evenNums.length)];
+        }
+
+        // chose diagonal corner spot
+        if (!board.cellFree(5) && freeCells.length === 7) {
+            switch(board.X[0]) {
+                case 2: return 8;
+                case 4: return 6;
+                case 8: return 2;
+                case 6: return 4;
+            }
+        }
+
+        // setup for trap (fork) and three corners
+        if (board.cellFree(5) && freeCells.length === 7) {
+            switch(board.X[0]) {
+                case 2:
+                    if (board.cellFree(4) && board.cellFree(9)) return 4;
+                    else return 6;
+                case 4:
+                    if (board.cellFree(2) && board.cellFree(9)) return 2;
+                    else return 8;
+                case 6:
+                    if (board.cellFree(8)&& board.cellFree(1)) return 8;
+                    else return 2;
+                case 8:
+                    if (board.cellFree(6)&& board.cellFree(1)) return 6;
+                    else return 4;
+            }
+        }
+
+        // finishes the trap (fork) or three corners
+        if (board.cellFree(5) && freeCells.length === 5) {
+            // check if edge or corner (for loop)
+            for (j = 0; j < evenNums.length; j++) {
+                if (board.O[0] === evenNums[j]) { // corner spot
+                    for (k = 0; freeCells.length; k++) {
+                        if (freeCells[k] % 2 === 0) {
+                            alert("corner kick");
+                            return freeCells[k];
                         }
                     }
                 }
             }
-            // if magicSum != 15 && board.O.length >= 2
-            // basically cats game from here
-            return freeCells[Math.floor(Math.random() * freeCells.length)];
-        } else { // X can do strategy
-            var evenNums = [2, 4, 6 ,8];
-
-            // nothing on the board, x makes random move in corner
-            if (freeCells.length === 9) {
-                return evenNums[Math.floor(Math.random() * evenNums.length)];
-            }
-
-            // chose diagonal corner spot
-            if (!board.cellFree(5) && freeCells.length === 7) {
-                switch(board.X[0]) {
-                    case 2: return 8;
-                    case 4: return 6;
-                    case 8: return 2;
-                    case 6: return 4;
-                }
-            }
-
-            // setup for trap (fork) or three corners
-            if (board.cellFree(5) && freeCells.length === 7) {
-                switch(board.X[0]) {
-                    case 2:
-                        if (board.cellFree(6)) return 6;
-                        else return 4;
-                    case 4:
-                        if (board.cellFree(2)) return 2;
-                        else return 8;
-                    case 6:
-                        if (board.cellFree(8)) return 8;
-                        else return 2;
-                    case 8:
-                        if (board.cellFree(6)) return 6;
-                        else return 4;
-                }
-            }
-
-            // finishes the trap (fork)
-            if (board.cellFree(5) && freeCells.length === 5) {
-                alert("im doing this");
-                return 5;
-            }
-
-            // finishes 3 corners
-            // logic is complete, don't even need this part.
-            //if (!board.cellFree(5) && freeCells.length === 5) {
-            //    for (i = 0; i < evenNums.length; i++) {
-            //        for (j = 0; j < freeCells.length; j++) {
-            //            if (freeCells[j] % 2 === evenNums[i]) return evenNums[i];
-            //        }
-            //    }
-            //}
+            alert("trap daddy");
+            return 5; // trap spot
         }
+
     } else { // agent 2 (also known as playerTwo)
-        if (board.cellFree(5)) return 5;
+        //if (board.cellFree(5)) return 5;
         return freeCells[Math.floor(Math.random() * freeCells.length)];
     }
 };
