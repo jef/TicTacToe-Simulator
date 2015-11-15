5d962e4268108e30f7c280036360164c0f71831b
Jeffrey LeCompte <jeffreylec@gmail.com>
2015-11-14 20:32:46 -0800

    All solutions are now solved. The game will either draw or win, for any given problem.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -8,11 +8,11 @@ var Agent = function () {
 
 };
 
-Agent.prototype.selectMove = function(board) {
+Agent.prototype.selectMove = function (board) {
     var freeCells = [];
     var magicSum;
-    var evenNums = [2, 4, 6 ,8];
-    var oddNums = [1, 3, 7 ,9];
+    var evenNums = [2, 4, 6, 8];
+    var oddNums = [1, 3, 7, 9];
     var f, i, k, l;
 
     for (i = 1; i < 10; i++)
@@ -21,6 +21,7 @@ Agent.prototype.selectMove = function(board) {
     if (board.playerOne) {
         magicSum = 0;
 
+        // random peice
         //return freeCells[Math.floor(Math.random() * freeCells.length)];
 
         // test to see if we can win
@@ -51,18 +52,24 @@ Agent.prototype.selectMove = function(board) {
         }
 
         // chose diagonal corner spot
-        if (!board.cellFree(5) && freeCells.length === 7) {
-            switch(board.X[0]) {
-                case 2: return 8;
-                case 4: return 6;
-                case 8: return 2;
-                case 6: return 4;
+        if (!board.cellFree(5) && board.X[0] % 2 === 0 && freeCells.length === 7) {
+            switch (board.X[0]) {
+                case 2:
+                    return 8;
+                case 4:
+                    return 6;
+                case 8:
+                    return 2;
+                case 6:
+                    return 4;
+                default:
+                    return freeCells[Math.floor(Math.random() * freeCells.length)];
             }
         }
 
         // setup for trap (fork) and three corners
-        if (board.cellFree(5) && freeCells.length === 7) {
-            switch(board.X[0]) {
+        if (board.cellFree(5) && board.X[0] % 2 === 0 && freeCells.length === 7) {
+            switch (board.X[0]) {
                 case 2:
                     if (board.cellFree(4) && board.cellFree(9)) return 4;
                     else return 6;
@@ -70,23 +77,108 @@ Agent.prototype.selectMove = function(board) {
                     if (board.cellFree(2) && board.cellFree(9)) return 2;
                     else return 8;
                 case 6:
-                    if (board.cellFree(8)&& board.cellFree(1)) return 8;
+                    if (board.cellFree(8) && board.cellFree(1)) return 8;
                     else return 2;
                 case 8:
-                    if (board.cellFree(6)&& board.cellFree(1)) return 6;
+                    if (board.cellFree(6) && board.cellFree(1)) return 6;
                     else return 4;
+                default:
+                    return freeCells[Math.floor(Math.random() * freeCells.length)];
+            }
+        }
+
+        // starts in center
+        if (board.X[0] === 5 && freeCells.length === 7) {
+            if (board.O[0] % 2 === 0) {
+                switch (board.O[0]) {
+                    case 2:
+                        return 8;
+                    case 4:
+                        return 6;
+                    case 6:
+                        return 4;
+                    case 8:
+                        return 2;
+                }
+            } else {
+                switch (board.O[0]) {
+                    case 1: return 2;
+                    case 3: return 6;
+                    case 7: return 4;
+                    case 9: return 8;
+                }
+            }
+        }
+
+        // starts in center
+        if (board.X[0] === 5 && freeCells.length === 5) {
+            if (board.O[0] % 2 === 0) {
+                switch (board.O[0]) {
+                    case 2:
+                        return 4;
+                    case 4:
+                        return 8;
+                    case 6:
+                        return 2;
+                    case 8:
+                        return 6;
+                }
+            } else {
+                if (board.X[1] % 2 != 0) {
+                    switch (board.X[1]) {
+                        case 1:
+                            return 8;
+                        case 3:
+                            return 4;
+                        case 7:
+                            return 6;
+                        case 9:
+                            return 2;
+                    }
+                } else {
+                    switch (board.O[1]) {
+                        case 2: return 1;
+                        case 4: return 8;
+                        case 6: return 1;
+                        case 8: return 4;
+                    }
+                }
             }
         }
 
         // finishes the trap (fork) or three corners
         if (board.cellFree(5) && freeCells.length === 5) {
             if (board.O[0] % 2 === 0) { // corner spot
-                for (k = 0; freeCells.length; k++) {
+                for (k = 0; k < freeCells.length; k++) {
                     if (freeCells[k] % 2 === 0)
                         return freeCells[k];
                 }
             }
             return 5; // trap spot
+        }
+
+        if (!board.cellFree(5) && freeCells.length === 5) {
+            for (k = 0; k < freeCells.length; k++) {
+                if (board.X[k] % 2 === 0) {
+                    switch (board.X[k]) {
+                        case 2:
+                            if (board.cellFree(4)) return 4;
+                            else return 6;
+                        case 4:
+                            if (board.cellFree(2)) return 2;
+                            else return 8;
+                        case 6:
+                            if (board.cellFree(8)) return 8;
+                            else return 2;
+                        case 8:
+                            if (board.cellFree(6)) return 6;
+                            else return 4;
+                    }
+                }
+            }
+        } else {
+            // automatic draw, doesn't matter where this move is.
+            return freeCells[Math.floor(Math.random() * freeCells.length)];
         }
     } else { // agent 2 (also known as playerTwo)
         magicSum = 0;
@@ -108,6 +200,7 @@ Agent.prototype.selectMove = function(board) {
                 for (f = 0; f < freeCells.length; f++) {
                     magicSum = board.X[k] + board.X[l] + freeCells[f];
                     if (magicSum === 15) {
+                        //alert(magicSum);
                         return freeCells[f];
                     }
                 }
@@ -123,32 +216,39 @@ Agent.prototype.selectMove = function(board) {
         // breaks a weird setup that playerOne might do to trick
         // playerTwo into a trap.
         if (freeCells.length === 6) {
-            if (board.X[0] % 2 != 0 && board.X[1] % 2 != 0) {
-                switch(board.X[0]) {
-                    case 1: if (board.X[1] === 3) return 8;
-                        if (board.X[1] === 7) return 6;
-                        //return oddNums[Math.floor(Math.random() * oddNums.length)];
-                        break;
-                    case 3: if (board.X[1] === 1) return 8;
-                        if (board.X[1] === 9) return 4;
-                        //return oddNums[Math.floor(Math.random() * oddNums.length)];
-                        break;
-                    case 7: if (board.X[1] === 1) return 6;
-                        if (board.X[1] === 9) return 2;
-                        //return oddNums[Math.floor(Math.random() * oddNums.length)];
-                        break;
-                    case 9: if (board.X[1] === 3) return 4;
-                        if (board.X[1] === 7) return 2;
-                        //return oddNums[Math.floor(Math.random() * oddNums.length)];
-                        break;
+            for (k = 0; k < freeCells.length - 1; k++) {
+                if (board.X[k] % 2 != 0 && board.X[k + 1] % 2 != 0) {
+                    switch (board.X[0]) {
+                        case 1:
+                            if (board.X[1] === 3) return 8;
+                            if (board.X[1] === 7) return 6;
+                            break;
+                        case 3:
+                            if (board.X[1] === 1) return 8;
+                            if (board.X[1] === 9) return 4;
+                            break;
+                        case 7:
+                            if (board.X[1] === 1) return 6;
+                            if (board.X[1] === 9) return 2;
+                            break;
+                        case 9:
+                            if (board.X[1] === 3) return 4;
+                            if (board.X[1] === 7) return 2;
+                            break;
+                    }
+                } else if (board.X[k] === 5 && board.X[k + 1] % 2 === 0 ||
+                    board.X[k + 1] === 5 && board.X[k] % 2 === 0) {
+                    return evenNums[Math.floor(Math.random() * evenNums.length)];
+                } else {
+                    return oddNums[Math.floor(Math.random() * oddNums.length)];
                 }
-            } else {
-                return oddNums[Math.floor(Math.random() * oddNums.length)];
             }
         } else {
             // picks an edge to try and force a cat's game
             if (board.X[0] % 2 === 0) {
                 return oddNums[Math.floor(Math.random() * oddNums.length)];
+            } else {
+                return freeCells[Math.floor(Math.random() * freeCells.length)];
             }
         }
     }
